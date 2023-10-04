29b111d8890c4bb995cb439f3e5fc38cb8c6359b
Anthony A <Argento-Finol-Anthony@hotmail.com>
2023-10-04 10:55:05 +0200

    Ajout Login

---
+++ GameDb/Tables/Favoris.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Favoris]
+(
+	Id_Jeu INT NOT NULL,
+	Id_User INT NOT NULL,
+    CONSTRAINT [FK_Favoris_Game] FOREIGN KEY ([Id_Jeu]) REFERENCES [Game]([Id]),
+	CONSTRAINT [FK_Favoris_User] FOREIGN KEY ([Id_User]) REFERENCES [UsersDB]([Id]),
+	CONSTRAINT PK_Favoris PRIMARY KEY (Id_Jeu, Id_User)
+)
